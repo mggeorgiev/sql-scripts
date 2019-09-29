bbc5a90d1247278b359da2590cd8a05181ef4038
Martin Georgiev Georgiev <mggeorgiev@gmail.com>
2019-09-29 21:04:42 +0300

    Update CreateCoreUser.sql

--- t_sql/CreateCoreUser.sql
+++ t_sql/CreateCoreUser.sql
@@ -1,3 +1,13 @@
+-- Drop the database 'DatabaseName'
+-- Connect to the 'master' database to run this snippet
+-- USE master
+-- GO
+
+-- DROP DATABASE [ec-th]
+-- GO
+
+-- DROP USER IF EXISTS ec_admin;
+-- DROP LOGIN ec_admin;
 -- Create a new database called 'ec-th'
 -- Connect to the 'master' database to run this snippet
 USE master
@@ -14,6 +24,20 @@ GO
 USE [ec-th]
 GO
 
-CREATE USER ec_app   
+-- Creates the login AbolrousHazem with password '340$Uuxwp7Mcxo7Khy'.  
+CREATE LOGIN ec_admin   
     WITH PASSWORD = '340$Uuxwp7Mcxo7Khy';  
 GO  
+
+-- Creates a database user for the login created above.  
+CREATE USER ec_admin FOR LOGIN ec_admin;  
+GO 
+
+
+
+-- Create User ec_admin For Login ec_admin with Default_Schema= [dbo];
+-- go
+-- USE [ec-th]
+-- GO
+-- EXEC sp_addrolemember 'db_ddladmin', N'ec_admin'
+-- go

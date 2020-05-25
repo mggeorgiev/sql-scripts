73a04f5e1581ed20c6c6a717d046c415a34aae25
Martin Georgiev <mggeorgiev@gmail.com>
2020-05-25 22:08:41 +0300

    Create restore_object.sql

---
+++ t_sql/DBAtools/restore_object.sql
@@ -0,0 +1,6 @@
+Select Convert(varchar(Max),Substring([RowLog Contents 0]
+,33
+,LEN([RowLog Contents 0]))) as [Script]
+from fn_dblog(NULL,NULL)
+Where [Operation]='LOP_DELETE_ROWS' And [Context]='LCX_MARK_AS_GHOST'
+And [AllocUnitName]='sys.sysobjvalues.clst'

e0480a8f4c73d94aef87a35424f5dbe2d21cf7b5
Martin Georgiev <mggeorgiev@gmail.com>
2021-08-17 16:42:13 +0300

    add filtering

--- t_sql/hierarchy/recursive_cte.sql
+++ t_sql/hierarchy/recursive_cte.sql
@@ -6,12 +6,14 @@ SELECT TOP (1000) [Departure]
 
 DECLARE @departure AS VARCHAR(255) = 'San Francisco';
 DECLARE @busname as VARCHAR(255) = 'Bus 1';
+DECLARE @maxlevel as INT = 5;
 
 WITH cte_route AS (
     SELECT       
         [Departure] 
         ,[BusName]
-        ,[Destination]   
+        ,[Destination]
+        ,0 as lvl
     FROM       
         [data_science].[network].[Trip] tp
     WHERE [Departure] = @departure AND  [BusName] = @busname and [Destination] <> @departure
@@ -19,13 +21,19 @@ WITH cte_route AS (
     SELECT 
         e.[Departure] 
         ,e.[BusName]
-        ,e.[Destination]  
+        ,e.[Destination]
+        ,o.lvl+1 
     FROM 
         cte_route AS o 
         INNER JOIN [data_science].[network].[Trip] AS e
             ON o.[Destination] = e.[Departure]
      WHERE o.[Destination] <> @departure
+     AND o.lvl<@maxlevel
 )
 
-SELECT * FROM cte_route OPTION (MAXRECURSION 100);
+SELECT 
+    * 
+    FROM cte_route
+    ORDER BY lvl 
+    OPTION (MAXRECURSION 100);
 GO

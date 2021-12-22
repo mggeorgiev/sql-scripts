22993a66b251129e47c75b5b93fb35271e28f4f1
Martin Georgiev <mggeorgiev@gmail.com>
2021-12-22 10:06:03 +0100

    Database Administrators

---
+++ t_sql/occupations/02-initial_data.sql
@@ -0,0 +1,32 @@
+-- skills
+INSERT INTO occupations.dbo.skills (Id, Skill, Description) VALUES(1, 'Critical Thinking', 'Using logic and reasoning to identify the strengths and weaknesses of alternative solutions, conclusions, or approaches to problems.');
+INSERT INTO occupations.dbo.skills (Id, Skill, Description) VALUES(2, 'Complex Problem Solving', 'Identifying complex problems and reviewing related information to develop and evaluate options and implement solutions.See more occupations related to this skill.');
+INSERT INTO occupations.dbo.skills (Id, Skill, Description) VALUES(3, 'Active Learning', 'Understanding the implications of new information for both current and future problem-solving and decision-making.See more occupations related to this skill.');
+INSERT INTO occupations.dbo.skills (Id, Skill, Description) VALUES(4, 'Active Listening', 'Giving full attention to what other people are saying, taking time to understand the points being made, asking questions as appropriate, and not interrupting at inappropriate times.See more occupations related to this skill.');
+INSERT INTO occupations.dbo.skills (Id, Skill, Description) VALUES(5, 'Judgment and Decision Making', 'Considering the relative costs and benefits of potential actions to choose the most appropriate one.');
+
+--abilities
+INSERT INTO occupations.dbo.abilities (Id, Ability, Description) VALUES(1, 'Deductive Reasoning', 'The ability to apply general rules to specific problems to produce answers that make sense.See more occupations related to this ability.');
+INSERT INTO occupations.dbo.abilities (Id, Ability, Description) VALUES(2, 'Information Ordering', 'The ability to arrange things or actions in a certain order or pattern according to a specific rule or set of rules (e.g., patterns of numbers, letters, words, pictures, mathematical operations).See more occupations related to this ability.');
+INSERT INTO occupations.dbo.abilities (Id, Ability, Description) VALUES(3, 'Problem Sensitivity', 'The ability to tell when something is wrong or is likely to go wrong. It does not involve solving the problem, only recognizing that there is a problem.See more occupations related to this ability.');
+INSERT INTO occupations.dbo.abilities (Id, Ability, Description) VALUES(4, 'Inductive Reasoning', 'The ability to combine pieces of information to form general rules or conclusions (includes finding a relationship among seemingly unrelated events).See more occupations related to this ability.');
+INSERT INTO occupations.dbo.abilities (Id, Ability, Description) VALUES(5, 'Oral Comprehension', 'The ability to listen to and understand information and ideas presented through spoken words and sentences.');
+
+--tasks
+INSERT INTO occupations.dbo.tasks (Id, Task) VALUES(1, 'Test programs or databases, correct errors, and make necessary modifications.See more occupations related to this task.');
+INSERT INTO occupations.dbo.tasks (Id, Task) VALUES(2, 'Plan, coordinate, and implement security measures to safeguard information in computer files against accidental or unauthorized damage, modification or disclosure.See more occupations related to this task.');
+INSERT INTO occupations.dbo.tasks (Id, Task) VALUES(3, 'Modify existing databases and database management systems or direct programmers and analysts to make changes.See more occupations related to this task.');
+INSERT INTO occupations.dbo.tasks (Id, Task) VALUES(4, 'Specify users and user access levels for each segment of database.See more occupations related to this task.');
+INSERT INTO occupations.dbo.tasks (Id, Task) VALUES(5, 'Write and code logical and physical database descriptions and specify identifiers of database to management system, or direct others in coding descriptions.');
+
+--detailed_work_activities
+
+INSERT INTO occupations.dbo.detailed_work_activities (Id, detailed_work_activity) VALUES(1, 'Assess database performance.See more occupations related to this activity.');
+INSERT INTO occupations.dbo.detailed_work_activities (Id, detailed_work_activity) VALUES(2, 'Modify software programs to improve performance.See more occupations related to this activity.');
+INSERT INTO occupations.dbo.detailed_work_activities (Id, detailed_work_activity) VALUES(3, 'Implement security measures for computer or information systems.See more occupations related to this activity.');
+INSERT INTO occupations.dbo.detailed_work_activities (Id, detailed_work_activity) VALUES(4, 'Develop computer or information security policies or procedures.See more occupations related to this activity.');
+INSERT INTO occupations.dbo.detailed_work_activities (Id, detailed_work_activity) VALUES(5, 'Create databases to store electronic data.');
+
+--work_activities
+
+

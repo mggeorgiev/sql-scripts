20a9e29627540484ce021c0c89def337dc5052a1
Martin Georgiev <mggeorgiev@gmail.com>
2021-12-22 11:59:03 +0100

    Web Developper

--- t_sql/occupations/02-initial_data.sql
+++ t_sql/occupations/02-initial_data.sql
@@ -1,16 +1,42 @@
+/*tasks*/
+INSERT INTO occupations.dbo.tasks (Id, Task) VALUES(1, 'Back up files from Web sites to local directories for instant recovery in case of problems.See more occupations related to this task.');
+INSERT INTO occupations.dbo.tasks (Id, Task) VALUES(2, 'Consult with management to ensure agreement on system principles.');
+INSERT INTO occupations.dbo.tasks (Id, Task) VALUES(3, 'Design, build, or maintain Web sites, using authoring or scripting languages, content creation tools, management tools, and digital media.See more occupations related to this task.');
+INSERT INTO occupations.dbo.tasks (Id, Task) VALUES(4, 'Evaluate code to ensure that it is valid, is properly structured, meets industry standards, and is compatible with browsers, devices, or operating systems.');
+INSERT INTO occupations.dbo.tasks (Id, Task) VALUES(5, 'Modify existing databases and database management systems or direct programmers and analysts to make changes.See more occupations related to this task.');
+INSERT INTO occupations.dbo.tasks (Id, Task) VALUES(6, 'Specify users and user access levels for each segment of database.See more occupations related to this task.');
+INSERT INTO occupations.dbo.tasks (Id, Task) VALUES(7, 'Plan, coordinate, and implement security measures to safeguard information in computer files against accidental or unauthorized damage, modification or disclosure.See more occupations related to this task.');
+INSERT INTO occupations.dbo.tasks (Id, Task) VALUES(8, 'Test, maintain, and monitor computer programs and systems, including coordinating the installation of computer programs and systems.See more occupations related to this task.');
+INSERT INTO occupations.dbo.tasks (Id, Task) VALUES(9, 'Test programs or databases, correct errors, and make necessary modifications.See more occupations related to this task.');
+INSERT INTO occupations.dbo.tasks (Id, Task) VALUES(10, 'Troubleshoot program and system malfunctions to restore normal functioning.See more occupations related to this task.');
+INSERT INTO occupations.dbo.tasks (Id, Task) VALUES(11, 'Expand or modify system to serve new purposes or improve work flow.See more occupations related to this task.');
+INSERT INTO occupations.dbo.tasks (Id, Task) VALUES(12, 'Use the computer in the analysis and solution of business problems, such as development of integrated production and inventory control and cost analysis systems.See more occupations related to this task.');
+INSERT INTO occupations.dbo.tasks (Id, Task) VALUES(13, 'Write supporting code for Web applications or Web sites.See more occupations related to this task.');
+INSERT INTO occupations.dbo.tasks (Id, Task) VALUES(14, 'Select programming languages, design tools, or applications.See more occupations related to this task.');
+INSERT INTO occupations.dbo.tasks (Id, Task) VALUES(15, 'Write and code logical and physical database descriptions and specify identifiers of database to management system, or direct others in coding descriptions.');
+
+/*knowledge*/
+
+INSERT INTO occupations.dbo.knowledge (Id, knowledge, Description) VALUES(1, 'Administrative', 'Knowledge of administrative and office procedures and systems such as word processing, managing files and records, stenography and transcription, designing forms, and workplace terminology.');
+INSERT INTO occupations.dbo.knowledge (Id, knowledge, Description) VALUES(2, 'Administration and Management', 'Knowledge of business and management principles involved in strategic planning, resource allocation, human resources modeling, leadership technique, production methods, and coordination of people and resources.');
+INSERT INTO occupations.dbo.knowledge (Id, knowledge, Description) VALUES(3, 'Communications and Media', 'Knowledge of media production, communication, and dissemination techniques and methods. This includes alternative ways to inform and entertain via written, oral, and visual media.');
+INSERT INTO occupations.dbo.knowledge (Id, knowledge, Description) VALUES(4, 'Computers and Electronics', 'Knowledge of circuit boards, processors, chips, electronic equipment, and computer hardware and software, including applications and programming.See more occupations related to this knowledge.');
+INSERT INTO occupations.dbo.knowledge (Id, knowledge, Description) VALUES(5, 'Customer and Personal Service', 'Knowledge of principles and processes for providing customer and personal services. This includes customer needs assessment, meeting quality standards for services, and evaluation of customer satisfaction.See more occupations related to this knowledge.');
+INSERT INTO occupations.dbo.knowledge (Id, knowledge, Description) VALUES(6, 'English Language', 'Knowledge of the structure and content of the English language including the meaning and spelling of words, rules of composition, and grammar.See more occupations related to this knowledge.');
+INSERT INTO occupations.dbo.knowledge (Id, knowledge, Description) VALUES(7, 'Mathematics', 'Knowledge of arithmetic, algebra, geometry, calculus, statistics, and their applications.See more occupations related to this knowledge.');
+INSERT INTO occupations.dbo.knowledge (Id, knowledge, Description) VALUES(8, 'Telecommunications', 'Knowledge of transmission, broadcasting, switching, control, and operation of telecommunications systems.See more occupations related to this knowledge.');
+
 /* skills*/
-INSERT INTO occupations.dbo.skills (Id, Skill, Description) VALUES(1, 'Critical Thinking', 'Using logic and reasoning to identify the strengths and weaknesses of alternative solutions, conclusions, or approaches to problems.');
-INSERT INTO occupations.dbo.skills (Id, Skill, Description) VALUES(2, 'Complex Problem Solving', 'Identifying complex problems and reviewing related information to develop and evaluate options and implement solutions.See more occupations related to this skill.');
-INSERT INTO occupations.dbo.skills (Id, Skill, Description) VALUES(3, 'Active Learning', 'Understanding the implications of new information for both current and future problem-solving and decision-making.See more occupations related to this skill.');
-INSERT INTO occupations.dbo.skills (Id, Skill, Description) VALUES(4, 'Active Listening', 'Giving full attention to what other people are saying, taking time to understand the points being made, asking questions as appropriate, and not interrupting at inappropriate times.See more occupations related to this skill.');
+INSERT INTO occupations.dbo.skills (Id, Skill, Description) VALUES(1, 'Active Learning', 'Understanding the implications of new information for both current and future problem-solving and decision-making.');
+INSERT INTO occupations.dbo.skills (Id, Skill, Description) VALUES(2, 'Active Listening', 'Giving full attention to what other people are saying, taking time to understand the points being made, asking questions as appropriate, and not interrupting at inappropriate times.');
+INSERT INTO occupations.dbo.skills (Id, Skill, Description) VALUES(3, 'Critical Thinking', 'Using logic and reasoning to identify the strengths and weaknesses of alternative solutions, conclusions, or approaches to problems.');
+INSERT INTO occupations.dbo.skills (Id, Skill, Description) VALUES(4, 'Complex Problem Solving', 'Identifying complex problems and reviewing related information to develop and evaluate options and implement solutions.');
 INSERT INTO occupations.dbo.skills (Id, Skill, Description) VALUES(5, 'Judgment and Decision Making', 'Considering the relative costs and benefits of potential actions to choose the most appropriate one.');
-
-INSERT INTO occupations.dbo.skills (Id, Skill, Description) VALUES(6, '', '');
-INSERT INTO occupations.dbo.skills (Id, Skill, Description) VALUES(7, '', '');
-INSERT INTO occupations.dbo.skills (Id, Skill, Description) VALUES(8, 'Reading Comprehension', 'Understanding written sentences and paragraphs in work-related documents.See more occupations related to this skill.');
-INSERT INTO occupations.dbo.skills (Id, Skill, Description) VALUES(9, 'Speaking', 'Talking to others to convey information effectively.See more occupations related to this skill.');
+INSERT INTO occupations.dbo.skills (Id, Skill, Description) VALUES(6, 'Operations Analysis', 'Analyzing needs and product requirements to create a design.');
+INSERT INTO occupations.dbo.skills (Id, Skill, Description) VALUES(7, 'Programming', 'Writing computer programs for various purposes.');
+INSERT INTO occupations.dbo.skills (Id, Skill, Description) VALUES(8, 'Reading Comprehension', 'Understanding written sentences and paragraphs in work-related documents.');
+INSERT INTO occupations.dbo.skills (Id, Skill, Description) VALUES(9, 'Speaking', 'Talking to others to convey information effectively.');
 INSERT INTO occupations.dbo.skills (Id, Skill, Description) VALUES(10, 'Systems Analysis', 'Determining how a system should work and how changes in conditions, operations, and the environment will affect outcomes.');
-
 
 /*abilities*/
 INSERT INTO occupations.dbo.abilities (Id, Ability, Description) VALUES(1, 'Deductive Reasoning', 'The ability to apply general rules to specific problems to produce answers that make sense.See more occupations related to this ability.');
@@ -18,21 +44,21 @@ INSERT INTO occupations.dbo.abilities (Id, Ability, Description) VALUES(2, 'Info
 INSERT INTO occupations.dbo.abilities (Id, Ability, Description) VALUES(3, 'Problem Sensitivity', 'The ability to tell when something is wrong or is likely to go wrong. It does not involve solving the problem, only recognizing that there is a problem.See more occupations related to this ability.');
 INSERT INTO occupations.dbo.abilities (Id, Ability, Description) VALUES(4, 'Inductive Reasoning', 'The ability to combine pieces of information to form general rules or conclusions (includes finding a relationship among seemingly unrelated events).See more occupations related to this ability.');
 INSERT INTO occupations.dbo.abilities (Id, Ability, Description) VALUES(5, 'Oral Comprehension', 'The ability to listen to and understand information and ideas presented through spoken words and sentences.');
-
 INSERT INTO occupations.dbo.abilities (Id, Ability, Description) VALUES(6, 'Written Comprehension', 'The ability to read and understand information and ideas presented in writing.See more occupations related to this ability.');
+INSERT INTO occupations.dbo.abilities (Id, Ability, Description) VALUES(7, 'Near Vision', 'The ability to see details at close range (within a few feet of the observer).See more occupations related to this ability.');
 
-/*tasks*/
-INSERT INTO occupations.dbo.tasks (Id, Task) VALUES(1, 'Test programs or databases, correct errors, and make necessary modifications.See more occupations related to this task.');
-INSERT INTO occupations.dbo.tasks (Id, Task) VALUES(2, 'Plan, coordinate, and implement security measures to safeguard information in computer files against accidental or unauthorized damage, modification or disclosure.See more occupations related to this task.');
-INSERT INTO occupations.dbo.tasks (Id, Task) VALUES(3, 'Modify existing databases and database management systems or direct programmers and analysts to make changes.See more occupations related to this task.');
-INSERT INTO occupations.dbo.tasks (Id, Task) VALUES(4, 'Specify users and user access levels for each segment of database.See more occupations related to this task.');
-INSERT INTO occupations.dbo.tasks (Id, Task) VALUES(5, 'Write and code logical and physical database descriptions and specify identifiers of database to management system, or direct others in coding descriptions.');
+/*work_activities*/
 
-INSERT INTO occupations.dbo.tasks (Id, Task) VALUES(6, 'Test, maintain, and monitor computer programs and systems, including coordinating the installation of computer programs and systems.See more occupations related to this task.');
-INSERT INTO occupations.dbo.tasks (Id, Task) VALUES(7, 'Troubleshoot program and system malfunctions to restore normal functioning.See more occupations related to this task.');
-INSERT INTO occupations.dbo.tasks (Id, Task) VALUES(8, 'Expand or modify system to serve new purposes or improve work flow.See more occupations related to this task.');
-INSERT INTO occupations.dbo.tasks (Id, Task) VALUES(9, 'Use the computer in the analysis and solution of business problems, such as development of integrated production and inventory control and cost analysis systems.See more occupations related to this task.');
-INSERT INTO occupations.dbo.tasks (Id, Task) VALUES(10, 'Consult with management to ensure agreement on system principles.');
+INSERT INTO occupations.dbo.work_activities (Id, Work_activity, Description) VALUES(1, 'Working with Computers', 'Using computers and computer systems (including hardware and software) to program, write software, set up functions, enter data, or process information.');
+INSERT INTO occupations.dbo.work_activities (Id, Work_activity, Description) VALUES(2, 'Processing Information', 'Compiling, coding, categorizing, calculating, tabulating, auditing, or verifying information or data.');
+INSERT INTO occupations.dbo.work_activities (Id, Work_activity, Description) VALUES(3, 'Analyzing Data or Information', 'Identifying the underlying principles, reasons, or facts of information by breaking down information or data into separate parts.');
+INSERT INTO occupations.dbo.work_activities (Id, Work_activity, Description) VALUES(4, 'Making Decisions and Solving Problems', 'Analyzing information and evaluating results to choose the best solution and solve problems.');
+INSERT INTO occupations.dbo.work_activities (Id, Work_activity, Description) VALUES(5, 'Getting Information', 'Observing, receiving, and otherwise obtaining information from all relevant sources.');
+
+INSERT INTO occupations.dbo.work_activities (Id, Work_activity, Description) VALUES(6, 'Communicating with Supervisors, Peers, or Subordinates', 'Providing information to supervisors, co-workers, and subordinates by telephone, in written form, e-mail, or in person.');
+
+INSERT INTO occupations.dbo.work_activities (Id, Work_activity, Description) VALUES(7, 'Thinking Creatively — Developing, designing, or creating new applications, ideas, relationships, systems, or products, including artistic contributions.See more occupations related to this activity.');
+INSERT INTO occupations.dbo.work_activities (Id, Work_activity, Description) VALUES(8, 'Updating and Using Relevant Knowledge — Keeping up-to-date technically and applying new knowledge to your job.See more occupations related to this activity.');
 
 /*detailed_work_activities*/
 
@@ -48,23 +74,8 @@ INSERT INTO occupations.dbo.detailed_work_activities (Id, detailed_work_activity
 INSERT INTO occupations.dbo.detailed_work_activities (Id, detailed_work_activity) VALUES(9, 'Troubleshoot issues with computer applications or systems.');
 INSERT INTO occupations.dbo.detailed_work_activities (Id, detailed_work_activity) VALUES(10, 'Modify software programs to improve performance.');
 
-/*work_activities*/
-
-INSERT INTO occupations.dbo.work_activities (Id, Work_activity, Description) VALUES(1, 'Working with Computers', 'Using computers and computer systems (including hardware and software) to program, write software, set up functions, enter data, or process information.');
-INSERT INTO occupations.dbo.work_activities (Id, Work_activity, Description) VALUES(2, 'Processing Information', 'Compiling, coding, categorizing, calculating, tabulating, auditing, or verifying information or data.');
-INSERT INTO occupations.dbo.work_activities (Id, Work_activity, Description) VALUES(3, 'Analyzing Data or Information', 'Identifying the underlying principles, reasons, or facts of information by breaking down information or data into separate parts.');
-INSERT INTO occupations.dbo.work_activities (Id, Work_activity, Description) VALUES(4, 'Making Decisions and Solving Problems', 'Analyzing information and evaluating results to choose the best solution and solve problems.');
-INSERT INTO occupations.dbo.work_activities (Id, Work_activity, Description) VALUES(5, 'Getting Information', 'Observing, receiving, and otherwise obtaining information from all relevant sources.');
-
-INSERT INTO occupations.dbo.work_activities (Id, Work_activity, Description) VALUES(6, 'Communicating with Supervisors, Peers, or Subordinates', 'Providing information to supervisors, co-workers, and subordinates by telephone, in written form, e-mail, or in person.');
-
-/*knowledge*/
-
-INSERT INTO occupations.dbo.knowledge (Id, knowledge, Description) VALUES(1, 'Computers and Electronics', 'Knowledge of circuit boards, processors, chips, electronic equipment, and computer hardware and software, including applications and programming.See more occupations related to this knowledge.');
-INSERT INTO occupations.dbo.knowledge (Id, knowledge, Description) VALUES(2, 'English Language', 'Knowledge of the structure and content of the English language including the meaning and spelling of words, rules of composition, and grammar.See more occupations related to this knowledge.');
-INSERT INTO occupations.dbo.knowledge (Id, knowledge, Description) VALUES(3, 'Telecommunications', 'Knowledge of transmission, broadcasting, switching, control, and operation of telecommunications systems.See more occupations related to this knowledge.');
-INSERT INTO occupations.dbo.knowledge (Id, knowledge, Description) VALUES(4, 'Mathematics', 'Knowledge of arithmetic, algebra, geometry, calculus, statistics, and their applications.See more occupations related to this knowledge.');
-INSERT INTO occupations.dbo.knowledge (Id, knowledge, Description) VALUES(5, 'Administrative', 'Knowledge of administrative and office procedures and systems such as word processing, managing files and records, stenography and transcription, designing forms, and workplace terminology.');
-
-INSERT INTO occupations.dbo.knowledge (Id, knowledge, Description) VALUES(6, 'Customer and Personal Service', 'Knowledge of principles and processes for providing customer and personal services. This includes customer needs assessment, meeting quality standards for services, and evaluation of customer satisfaction.See more occupations related to this knowledge.');
-INSERT INTO occupations.dbo.knowledge (Id, knowledge, Description) VALUES(7, 'Administration and Management', 'Knowledge of business and management principles involved in strategic planning, resource allocation, human resources modeling, leadership technique, production methods, and coordination of people and resources.');
+INSERT INTO occupations.dbo.detailed_work_activities (Id, detailed_work_activity) VALUES(11, 'Design websites or web applications.See more occupations related to this activity.');
+INSERT INTO occupations.dbo.detailed_work_activities (Id, detailed_work_activity) VALUES(12, 'Write computer programming code.See more occupations related to this activity.');
+INSERT INTO occupations.dbo.detailed_work_activities (Id, detailed_work_activity) VALUES(13, 'Update website content.See more occupations related to this activity.');
+INSERT INTO occupations.dbo.detailed_work_activities (Id, detailed_work_activity) VALUES(14, 'Create electronic data backup to prevent loss of information.See more occupations related to this activity.');
+INSERT INTO occupations.dbo.detailed_work_activities (Id, detailed_work_activity) VALUES(15, 'Test software performance.');

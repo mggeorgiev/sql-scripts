082aa60db71dcdd1a3af42c157f86b72ea0ed0ca
Martin Georgiev <mggeorgiev@gmail.com>
2022-05-21 23:14:28 +0200

    add blotto

--- t_sql/warmathematics/blotto.sql
+++ t_sql/warmathematics/blotto.sql
@@ -1,16 +1,3 @@
--- Create a new database called 'warmathematics'
--- Connect to the 'master' database to run this snippet
-USE master
-GO
--- Create the new database if it does not exist already
-IF NOT EXISTS (
-    SELECT [name]
-        FROM sys.databases
-        WHERE [name] = N'warmathematics'
-)
-CREATE DATABASE warmathematics
-GO
-
 use warmathematics
 GO
 
